e3fa850f51921c4b4998fd02a514acc589d4ea70
NKCuongz1901 <cuong19012002@gmail.com>
2026-02-26 00:03:11 +0700

    chore: base authen api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
+import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { PrismaService } from './prisma/prisma.service';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    AccountModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
