bac35f263bc4dc64cfe58eccecf0a3001f1b7d91
NKCuongz1901 <cuong19012002@gmail.com>
2026-03-01 20:22:38 +0700

    feat: finish accepted and reject pt request

--- backend/src/personal-trainer/personal-trainer.controller.ts
+++ backend/src/personal-trainer/personal-trainer.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { PersonalTrainerService } from './personal-trainer.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
@@ -16,5 +16,19 @@ export class PersonalTrainerController {
   @Get('requested-packages')
   async getRequestedPackages(@Req() req: any) {
     return this.personalTrainerService.getRequestedPackages(req.user.userId);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.PT)
+  @Post('accepted-request/:id')
+  async acceptedRequest(@Req() req: any, @Param('id') id: string) {
+    return this.personalTrainerService.acceptedRequest(req.user.userId, id);
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.PT)
+  @Post('rejected-request/:id')
+  async rejectedRequest(@Req() req: any, @Param('id') id: string) {
+    return this.personalTrainerService.rejectedRequest(req.user.userId, id);
   }
 }
--- backend/src/personal-trainer/personal-trainer.service.ts
+++ backend/src/personal-trainer/personal-trainer.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserPackageStatus } from 'generated/prisma/enums';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { calcEndAt } from 'src/utils/helpers';
 
 @Injectable()
 export class PersonalTrainerService {
   constructor(private readonly prisma: PrismaService) {}
+
   async getRequestedPackages(ptAccountId: string) {
     const listRequestedPackages = await this.prisma.userPackage.findMany({
       where: {
@@ -16,6 +18,57 @@ export class PersonalTrainerService {
     return {
       message: 'Get requested packages successfully',
       data: listRequestedPackages,
+    };
+  }
+
+  async acceptedRequest(ptAccountId: string, userPackageId: string) {
+    const requestedPackage = await this.prisma.userPackage.findUnique({
+      where: {
+        id: userPackageId,
+        ptAccountId: ptAccountId,
+        status: UserPackageStatus.PENDING,
+      },
+      include: {
+        package: true,
+      },
+    });
+    if (!requestedPackage) {
+      throw new NotFoundException('Requested package not found');
+    }
+    const now = new Date();
+    const endAt = calcEndAt(
+      now,
+      requestedPackage.package.unit,
+      requestedPackage.package.durationValue,
+    );
+    const updatedRequestedPackage = await this.prisma.userPackage.update({
+      where: { id: userPackageId },
+      data: {
+        status: UserPackageStatus.ACTIVE,
+        startAt: now,
+        endAt: endAt,
+        activatedAt: now,
+        expiredAt: endAt,
+      },
+    });
+    return {
+      message: 'Accepted request successfully',
+      data: updatedRequestedPackage,
+    };
+  }
+
+  async rejectedRequest(ptAccountId: string, userPackageId: string) {
+    const requestedPackage = await this.prisma.userPackage.update({
+      where: {
+        id: userPackageId,
+        ptAccountId: ptAccountId,
+        status: UserPackageStatus.PENDING,
+      },
+      data: { status: UserPackageStatus.REJECTED },
+    });
+    return {
+      message: 'Rejected request successfully',
+      data: requestedPackage,
     };
   }
 }
